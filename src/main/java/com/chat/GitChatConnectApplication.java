68cecff934332fdf685362c349e341e5919e4b49
Prakash A <prakashaathi321@gmail.com>
2023-08-31 19:04:08 +0530

    clean up

--- src/main/java/com/chat/GitChatConnectApplication.java
+++ src/main/java/com/chat/GitChatConnectApplication.java
@@ -2,21 +2,14 @@ package com.chat;
 
 import java.nio.file.Path;
 
-import javax.annotation.PostConstruct;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.cassandra.CqlSessionBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.chat.config.DataStaxAstraProperties;
 
 
 @SpringBootApplication
-@RestController
-// @RequiredArgsConstructor
 public class GitChatConnectApplication {
 
 
